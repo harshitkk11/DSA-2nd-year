2e3ecf1b37a8534e17962553f95ebd817eb1f0b6
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2022-09-22 00:01:50 +0530

    Create sum_of_digits.c

---
+++ Assignment2/sum_of_digits.c
@@ -0,0 +1,23 @@
+//   HackerRank:- https://www.hackerrank.com/challenges/sum-of-digits-of-a-five-digit-number/problem?isFullScreen=true
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n, a, sum=0;
+    scanf("%d", &n);
+    
+    while(n!=0){
+        a=n%10;
+        sum += a;
+        n= n/10;
+        
+    }
+    printf("%d", sum);
+    
+    return 0;
+}
