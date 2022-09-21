7b570f1b7101cbcf2e4af4f2ecbea2961909b6ca
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2022-09-22 00:34:26 +0530

    Create 1D-Array.c

---
+++ Assignment2/1D-Array.c
@@ -0,0 +1,24 @@
+// HackerRank:- https://www.hackerrank.com/challenges/1d-arrays-in-c/problem?isFullScreen=true
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n, sum=0;
+    scanf("%d", &n);
+    int *arr = (int*)malloc(n*sizeof(int));
+    
+    for (int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    for(int i=0; i<n; i++){
+        sum+=arr[i];
+    }
+    printf("%d", sum);
+    
+    return 0;
+}
