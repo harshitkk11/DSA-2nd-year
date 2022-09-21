9988374d17c1b10c1056acf2aef3933db5637be9
harshitkk11 <114001877+harshitkk11@users.noreply.github.com>
2022-09-21 12:51:43 +0530

    Create input and output.cpp

---
+++ Assignment1/input and output.cpp
@@ -0,0 +1,13 @@
+// HackerRank:- https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int num1, num2, num3;
+    cin >> num1>>num2>>num3;
+    int add = num1+num2+num3;
+    cout<< add;
+    return 0;
+}
